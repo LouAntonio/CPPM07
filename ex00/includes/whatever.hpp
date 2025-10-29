2d04fd9c81d12734a8d1041492087c786b863a9e
LouAntonio <lourencodala88@gmail.com>
2025-10-29 13:34:54 +0100

    removing unnecessary files and adding whatever

--- ex00/includes/whatever.hpp
+++ ex00/includes/whatever.hpp
@@ -1,20 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   min.cpp                                            :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/28 13:41:21 by lantonio          #+#    #+#             */
-/*   Updated: 2025/10/28 14:40:44 by lantonio         ###   ########.fr       */
+/*   Created: 2025/10/28 13:39:12 by lantonio          #+#    #+#             */
+/*   Updated: 2025/10/29 13:22:49 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Min.hpp"
+#ifndef SWAP_HPP
+#define SWAP_HPP
 
-void    min(T a, T b)
-{
-	if (a < b)
-        return a;
-    return b;
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b) {
+	T c = a;
+	a = b;
+	b = c;
 }
+
+template <typename T>
+T min(T a, T b)
+{
+	return a < b ? a : b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return a > b ? a : b;
+}
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,14 +6,11 @@
 /*   By: lantonio <lantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 13:45:39 by lantonio          #+#    #+#             */
-/*   Updated: 2025/10/28 14:40:22 by lantonio         ###   ########.fr       */
+/*   Updated: 2025/10/29 13:33:45 by lantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include "../includes/Swap.hpp"
-#include "../includes/Min.hpp"
-#include "../includes/Max.hpp"
+#include "../includes/whatever.hpp"
 
 int main(void)
 {
@@ -21,4 +18,33 @@ int main(void)
 	int b = 3;
 	::swap(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+	std::cout << "\n\n--- *** ---" << std::endl;
+	std::cout << "More Tests" << std::endl;
+	std::cout << "--- *** ---" << std::endl;
+	int	e = 12;
+	int	f = 12;
+	::swap(a, b);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min(e, f) << std::endl;
+	std::cout << "max( e, f ) = " << ::max(e, f) << std::endl;
+
+	std::cout << "\n\n--- *** ---" << std::endl;
+	std::cout << "More Tests" << std::endl;
+	std::cout << "--- *** ---" << std::endl;
+	std::string	g = "Lou";
+	std::string	h = "Antonio";
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min(g, h ) = " << ::min(g, h) << std::endl;
+	std::cout << "max(g, h ) = " << ::max(g, h) << std::endl;
+	return 0;
 }
